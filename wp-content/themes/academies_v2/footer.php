55d4fd63b4a251dce6a46859b8e11baa64cde6d7
Adam <adam@tealemonade.us>
2018-07-11 14:02:17 -0600

    Privacy Policy

--- wp-content/themes/academies_v2/footer.php
+++ wp-content/themes/academies_v2/footer.php
@@ -29,7 +29,7 @@
         <?php endif; ?>
         <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
        <p class="copyright">
-        &copy;<?php echo date("Y");  ?> <?php the_field('copyright', 'option'); ?> 
+        &copy;<?php echo date("Y");  ?> <?php the_field('copyright', 'option'); ?> | <a href="/privacy-policy/">Privacy Policy</a>
          </p>
       </div><!-- .site-info -->
      
